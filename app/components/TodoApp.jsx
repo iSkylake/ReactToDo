84ff742851d76b362634d0de6495ebc258588744
Llorch <skyakebeta@gmail.com>
2016-06-08 14:17:33 -0700

    Add complete toggle support and test

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -14,19 +14,23 @@ var TodoApp = React.createClass({
 			todos: [
 			{
 				id: uuid(),
-				text: 'Walk the dog'
+				text: 'Walk the dog',
+				completed: false
 			},
 			{
 				id: uuid(),
-				text: 'Clean the yard'
+				text: 'Clean the yard',
+				completed: true
 			},
 			{
 				id: uuid(),
-				text: 'Cook lunch'
+				text: 'Cook lunch',
+				completed: true
 			},
 			{
 				id: uuid(),
-				text: 'Collect some apples'
+				text: 'Collect some apples',
+				completed: false
 			}
 			]
 		};
@@ -38,9 +42,19 @@ var TodoApp = React.createClass({
 				{
 					id: uuid(),
 					text: text,
+					completed: false
 				}
 			]
 		});
+	},
+	handleToggle: function(id){
+		var updatedTodos = this.state.todos.map((todo) => {
+			if(todo.id === id){
+				todo.completed = !todo.completed;
+			}
+			return todo;
+		});
+		this.setState({todos: updatedTodos});
 	},
 	handleSearch: function(showCompleted, searchTest){
 		this.setState({
@@ -53,7 +67,7 @@ var TodoApp = React.createClass({
 		return(
 			<div>
 				<TodoSearch onSearch={this.handleSearch}/>
-				<TodoList todos={todos}/>
+				<TodoList todos={todos} onToggle={this.handleToggle}/>
 				<AddTodo onAddTodo={this.handleAddTodo}/>
 			</div>
 		);
--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -1,11 +1,28 @@
 var React = require('react'),
 	ReactDOM = require('react-dom'),
-	expect = require('expect');
+	expect = require('expect'),
+	TestUtils = require('react-addons-test-utils'),
+	$ = require('jQuery');
 
 var Todo = require('Todo');
 
 describe('Todo', () => {
 	it('should exist', () => {
 		expect(Todo).toExist();
+	});
+
+	it('should call onToggle prop with id on click', () => {
+		var todoData = {
+			id: 199,
+			text: 'Write todo.test.jsx test',
+			completed: true
+		};
+		var spy = expect.createSpy();
+		var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}/>);
+		var $el = $(ReactDOM.findDOMNode(todo));
+
+		TestUtils.Simulate.click($el[0]);
+
+		expect(spy).toHaveBeenCalledWith(199);
 	});
 });
